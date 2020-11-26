c7e5c1621f92ba8f6a00716c0f0d53ba6c480f67
Loïc Paulevé <loic.pauleve@ens-cachan.org>
2020-11-26 09:41:32 +0100

    add auto conversion from ginsim and biolqm objects with names

--- bns.py
+++ bns.py
@@ -5,26 +5,62 @@ the computation of attractors in synchronous Boolean Networks.
 import io
 import subprocess
 import pandas as pd
+import sys
 
 from colomoto.types import *
+from colomoto_jupyter.sessionfiles import *
+
+class BNSModel(object):
+    def __init__(self, filename, names=None):
+        self.filename = filename
+        self.names = names
+        # TODO: if names is None, try sniffing them from comments
+
+def _get_model(obj):
+    if isinstance(obj, BNSModel):
+        return obj
+
+    def biolqm_import(biolqm, obj):
+        cnetfile = new_output_file(".cnet")
+        assert biolqm.save(obj, cnetfile)
+        names = [str(n) for n in obj.getComponents()]
+        return BNSModel(cnetfile, names)
+
+    if "biolqm" in sys.modules:
+        biolqm = sys.modules["biolqm"]
+        if biolqm.is_biolqm_object(obj):
+            return biolqm_import(biolqm, obj)
+    if "ginsim" in sys.modules:
+        ginsim = sys.modules["ginsim"]
+        if ginsim.is_ginsim_object(obj):
+            biolqm = import_colomoto_tool("biolqm")
+            obj = ginsim.to_biolqm(obj)
+            return biolqm_import(biolqm, obj)
+    assert isinstance(obj, str), "unsupported model input"
+    return BNSModel(obj)
 
 
-def fixpoints(filename):
+def load(filename):
+    return _get_model(filename)
+
+def fixpoints(model):
     """
     Compute the fixed points of the given Boolean network `bn`.
 
-    `filename` should be the path to a file in CNET format.
+    `model` should be the path to a file in CNET format or a bioLQM or GINsim
+    object
 
     Returns None or a list of ``colomoto.types.State``.
     """
-    return attractors(filename, length=1)
+    return attractors(model, length=1)
 
 
-def attractors(filename, length=None):
+def attractors(model, length=None):
     """
     Compute the attractors of the given Boolean network `bn`.
 
-    `filename` should be the path to a file in CNET format.
+    `model` should be the path to a file in CNET format or a bioLQM or GINsim
+    object
 
     `length` enables to search only attractors of a specific length (or its factors)
 
@@ -33,12 +69,12 @@ def attractors(filename, length=None):
     otherwise a list of ``colomoto.types.HypercubeCollection``.
     """
 
-    # TODO: automagic conversion for some foreign models
-    
+    model = _get_model(model)
+
     args = [ "bns" ]
     if length:
         args += [  "-l", str(length) ]
-    args += [ filename ]
+    args += [ model.filename ]
 
     cmd = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
 
@@ -58,14 +94,16 @@ def attractors(filename, length=None):
 
     if max_len == 0:
         return None
-    
-    return [ _as_states(cur, nvar) for cur in attractors ]
+
+    return [ _as_states(cur, nvar, model.names) for cur in attractors ]
 
 
-def _as_states(attr, nvar):
+def _as_states(attr, nvar, names):
     widths = [ 1 for _ in range(nvar) ]
     with io.StringIO(attr) as stream:
         df = pd.read_fwf(stream, widths=widths, header=None)
+        if names:
+            df.columns = names
         if df.shape[0] == 1:
             return State(df.to_dict(orient="records")[0])
         return HypercubeCollection(df.to_dict(orient="records"))
